d08c9e2db4b1f0391d1de7ba21478051be134609
Russel Marcelo <russelmarcelo1509@gmail.com>
2023-07-12 01:17:29 +0200

    Initial Comment

--- src/ub10_wildcards/b10a2/GenericUtil.java
+++ src/ub10_wildcards/b10a2/GenericUtil.java
@@ -31,28 +31,5 @@ public abstract class GenericUtil {
         }
         return result;
     }
-
-    public static <T> Folge<T> getMinima(Puffer<? extends T> puffer1, Puffer<? extends T> puffer2, Comparator<? super T> comp) {
-
-        Folge<T> result = new FolgeMitDynArray<>();
-        if (puffer1.isEmpty() && puffer2.isEmpty()) {
-            return result;
-        }
-
-        Iterator<? extends T> i1 = puffer1.iterator();
-        Iterator<? extends T> i2 = puffer2.iterator();
-        while (i1.hasNext() && i2.hasNext()) {
-            T value1 = i1.next();
-            T value2 = i2.next();
-            result.insert(comp.compare(value1, value2) <= 0 ? value1 : value2);
-        }
-        while (i1.hasNext()) {
-            result.insert(i1.next());
-        }
-        while (i2.hasNext()) {
-            result.insert(i2.next());
-        }
-        return result;
-    }
-
+    
 }
